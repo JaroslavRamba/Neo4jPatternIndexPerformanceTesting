b082e82052764787d0450643dc2489be3e90467f
Martin Troup <troupmar@gmail.com>
2015-04-05 00:38:43 +0200

    Performance tests added.

---
+++ src/test/java/PerformanceTestQueries.java
@@ -0,0 +1,18 @@
+import com.graphaware.test.performance.PerformanceTest;
+import com.graphaware.test.performance.PerformanceTestSuite;
+
+public class PerformanceTestQueries extends PerformanceTestSuite {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected PerformanceTest[] getPerfTests() {
+        return new PerformanceTest[]{
+                //new GetTrianglesWithSingleNodePTest()
+                //new GetTrianglesWithAllNodesPTest()
+                new GetTrianglesDBSinglePTest()
+        };
+    }
+
+}
